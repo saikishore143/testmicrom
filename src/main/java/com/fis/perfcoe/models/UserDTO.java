59e6a8a5fda7a6e52ea8610d1bce028c22a38540
saikishore143 <33083787+saikishore143@users.noreply.github.com>
2018-08-21 15:07:43 +0530

    Add files via upload

---
+++ src/main/java/com/fis/perfcoe/models/UserDTO.java
@@ -0,0 +1,38 @@
+package com.fis.perfcoe.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserDTO {
+	  
+
+	    private String name;
+	    public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+
+		private String message   ;
+
+	
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+	
+
+
+	}
+}
