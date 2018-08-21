991fbaed87897e86c79b7f47ecf8006f28c7bf3c
saikishore143 <33083787+saikishore143@users.noreply.github.com>
2018-08-21 15:18:53 +0530

    Update UserRepository.java

--- src/main/java/com/fis/perfcoe/UserRepository.java
+++ src/main/java/com/fis/perfcoe/UserRepository.java
@@ -2,6 +2,6 @@ package com.fis.perfcoe;
 import org.springframework.data.repository.CrudRepository;
 
 import com.fis.perfcoe.models.UserDTO;
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<UserDTO, String> {
 
 }
