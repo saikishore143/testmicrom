45b0da37acb14826b5c8f85dcc63ea02ae7b0588
saikishore143 <33083787+saikishore143@users.noreply.github.com>
2018-10-23 11:07:52 +0530

    Delete UserRepository.java

--- src/main/java/com/fis/perfcoe/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fis.perfcoe;
-import org.springframework.data.repository.CrudRepository;
-
-import com.fis.perfcoe.models.UserDTO;
-public interface UserRepository extends CrudRepository<UserDTO, String> {
-
-}
